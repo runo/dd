1b9f79049caa205609188d51afdad27ddb794c2e
runo <runoyu@gmail.com>
2011-01-05 07:32:04 +0800

    nothing to say again :)

---
+++ test.c
@@ -0,0 +1,26 @@
+// 
+// test.c
+// ~/rep/github/dd/test.c
+// 
+// $ gcc -std=c99 -Wall -g -p -o ./test ./test.c
+// 
+// gbs, emx, x
+// 
+// runo
+// 1/5/2011
+// 
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    // put your code here :)
+
+    fprintf(stdout, "the quick brown fox jumps over the lazy dog :)");
+
+    return (0);
+}
+
+//:~
